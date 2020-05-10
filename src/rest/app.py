00608e1d777e8049ee22db0485ae3cdead5dea7c
AmroNagdy <amro.nagdy@gmail.com>
2020-05-10 20:16:05 +0100

    Small changes to DB data loading.

--- src/rest/app.py
+++ src/rest/app.py
@@ -10,7 +10,7 @@ NAME_KEY = 'primaryName'
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://ReadOnly:ReadOnly@cluster0-oboxf.mongodb.net/imdbActorNetworkVisualiserDB'
-app.config['MONGO_DBNAME'] = 'movies'
+app.config['MONGO_DBNAME'] = 'imdbActorNetworkVisualiserDB'
 
 mongo = PyMongo(app)
 movies_collection = mongo.db['movies']
@@ -24,11 +24,7 @@ def get_actor_network():
     for movie in movies:
         actors = movie[ACTORS_KEY]
         
-        for actor_a, actor_b in itertools.combinations(actors, 2):
-            nconst_a, nconst_b = actor_a[NCONST_KEY], actor_b[NCONST_KEY]
-
-            # Update link between two actors.
-            link = (nconst_a, nconst_b)
+        for link in itertools.combinations(actors, 2):
             if link not in links.keys():
                 links[link] = 1
             else:
@@ -39,12 +35,15 @@ def get_actor_network():
         for nconst in link:
             if nconst not in nconst_to_name.keys():
                 actor_details = actors_collection.find_one({'nconst': nconst})
-                name = actor_details[NAME_KEY]
-                nconst_to_name[nconst] = name
+
+                if actor_details is not None:
+                    name = actor_details[NAME_KEY]
+                    nconst_to_name[nconst] = name
+    available_nconsts = nconst_to_name.keys()
 
     network = {
         'nodes': [ { 'id': nconst, 'name': name } for nconst, name in nconst_to_name.items() ],
-        'links': [ { 'source': link[0], 'target': link[1], 'weight': weight } for link, weight in links.items() ]
+        'links': [ { 'source': link[0], 'target': link[1], 'weight': weight } for link, weight in links.items() if link[0] in available_nconsts and link[1] in available_nconsts ]
     }
 
     return dumps(network)
